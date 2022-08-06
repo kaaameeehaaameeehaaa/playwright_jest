d1385f13e1fbeec5bde18afc9ee54323d148a7e8
nerxxxy <nerxxxxy@gmail.com>
2022-08-06 22:27:46 +0300

    .

--- __tests__/01_basics.spec.js
+++ __tests__/01_basics.spec.js
@@ -1,16 +1,25 @@
 const {addAttach} = require("jest-html-reporters");
+Login = require('../pageObjects/login');
+
+
 describe('Post', () => {
+
+    const login = new Login()
+
     beforeAll(async () => {
         await page.goto('https://react-redux.realworld.io/#/login')
     })
+
+
     test.jestPlaywrightSkip({browsers:['webkit']},'Sign In' ,async () => {
         //await page.goto('https://react-redux.realworld.io/#/login')
         const title = await page.title()
         expect(title).toBe('Conduit')
 
-        await page.fill('input[type = "email"]', 'nerxtest@gmail.com')
-        await page.press('input[type = "email"]' , 'Tab')
-        await page.type('input[type = "password"]', 'nerxtest')
+        const email = await login.email()
+        await email.fill('nerxtest@gmail.com')
+        await email.press('Tab')
+        await login.password_fill('nerxtest')
 
         await Promise.all([
             page.waitForNavigation(),
